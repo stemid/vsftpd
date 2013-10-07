eca9cf6fc673ddcffcf1ec41d45aa961d87b4920
Stefan Midjich <swehack@gmail.com>
2013-10-07 13:25:12 +0200

    first test of user driver.

--- Driver/User.py
+++ Driver/User.py
@@ -19,54 +19,59 @@ class User:
 
         # Init system commands here to catch possible exceptions early
         self._useradd = sh.Command(s.useradd_binary)
+        self._userdel = sh.Command(s.userdel_binary)
         self._id = sh.Command(s.id_binary)
         self._setquota = sh.Command(s.setquota_binary)
 
     def _db_is_user(self, username):
         c = self._c
         rows = 0
-        c.execute('''
-                  select %{db_users_id} 
-                  from %{db_table_users} 
-                  where %{db_users_name}='%{username}'
-                  '''
-                 ).format(
-                     db_users_id = s.db_users_id,
-                     db_table_users = s.db_table_users,
-                     username
-                 )
+        rows = c.execute(
+            '''
+            select %{db_users_id} 
+            from %{db_table_users} 
+            where %{db_users_name}='%{username}'
+            '''
+        ).format(
+            db_users_id = s.db_users_id,
+            db_table_users = s.db_table_users,
+            username = username
+        )
+        return rows
 
     def _db_add_user(self, username, password):
         c = self._c
-        c.execute('''
-                  insert into %{db_table_users} 
-                  (%{db_users_name}, %{db_users_password})
-                  values ('%{username}', md5('%{password}'))
-                  '''
-                 ).format(
-                     db_table_users = s.db_table_users,
-                     db_users_name = s.db_users_name,
-                     db_users_password = s.db_users_password,
-                     username = username,
-                     password = password
-                 )
+        c.execute(
+            '''
+            insert into %{db_table_users} 
+            (%{db_users_name}, %{db_users_password})
+            values ('%{username}', md5('%{password}'))
+            '''
+        ).format(
+            db_table_users = s.db_table_users,
+            db_users_name = s.db_users_name,
+            username = username,
+            password = password
+        )
         self._db.commit()
 
     def _db_del_user(self, username):
         c = self._c
-        c.execute('''
-                  delete from %{db_table_users}
-                  where username='%{username}'
-                  '''
-                 )
+        c.execute(
+            '''
+            delete from %{db_table_users}
+            where username = '%{username}'
+            '''
+        ).format(
+            db_table_users = s.db_table_users,
+            username = username
+        )
 
     def _sys_is_user(self, username):
-        id = sh.Command(s.id_binary)
-        id(username)
+        return self._id(username)
 
     def _sys_add_user(self, username, home_dir):
-        useradd = sh.Command(s.useradd_binary)
-        useradd(
+        self._useradd(
             '-d',
             home_dir,
             '-m',
@@ -78,3 +83,48 @@ class User:
         userdel(username)
 
     def adduser(self, username, password, home=None, groups=None):
+        db_users = self._db_is_user(username)
+        if db_users >= 1:
+            raise UserError('User already exists in DB')
+
+        try:
+            sys_user = self._sys_is_user(username)
+        except sh.ErrorReturnCode_1 as e:
+            raise UserError('User already exists in system')
+        except Exception as e:
+            raise UserError('User could not be created')
+
+        try:
+            self._db_add_user(username, password)
+        except:
+            raise UserError('User could not be created in DB')
+
+        try:
+            self._sys_add_user(username, home, groups)
+        except:
+            self._db_del_user(username)
+            raise UserError('User could not be created in system')
+
+    def deluser(self, username):
+        (exc_str1, exc_str2) = (None, None)
+        try:
+            self._db_del_user(username)
+        except Exception as e:
+            exc_str1 = str(e)
+            pass
+        try:
+            self._sys_del_user(username)
+        except Exception as e:
+            exc_str2 = str(e)
+            pass
+        
+        if exc_str1 not None or exc_str2 not None:
+            return (exc_str1, exc_str2)
+        return True
+
+class UserError(Exception):
+    def __init__(self, errstr):
+        self.errstr = errstr
+
+    def __str__(self):
+        return repr(self.errstr)

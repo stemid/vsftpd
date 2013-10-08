e7fa2aee6282fd7517fd8d5809834775817c629d
Stefan Midjich <swehack@gmail.com>
2013-10-08 15:09:04 +0200

    second commit

--- tools/adduser.py
+++ tools/adduser.py
@@ -17,40 +17,32 @@ parser = optparse.OptionParser(
 
 parser.add_option(
     '-g', '--groups',
-    nargs = 1,
     action = 'store',
     type = 'string',
-    default = None,
     metavar = 'sudo,staff',
     help = 'Comma-separated list of additional group memberships'
 )
 
 parser.add_option(
     '-d', '--directory',
-    nargs = 1,
     action = 'store',
     type = 'string',
-    default = None,
     metavar = '/home/user',
     help = 'Home directory of user'
 )
 
 parser.add_option(
     '-p', '--password',
-    nargs = 1,
     action = 'store',
     type = 'string',
-    default = None,
     metavar = 'Secret2013',
     help = 'Password of user'
 )
 
 parser.add_option(
     '-u', '--username',
-    nargs = 1,
     action = 'store',
     type = 'string',
-    default = '',
     metavar = 'user',
     help = 'Users login name'
 )

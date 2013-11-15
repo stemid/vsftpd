a4d0a5e0f090b59a993d99aa5139606058e8cb40
Stefan Midjich <swehack@gmail.com>
2013-11-15 09:53:28 +0100

    debug

--- tools/adduser.py
+++ tools/adduser.py
@@ -5,68 +5,61 @@ from sys import path, exit, stderr
 from os.path import join, abspath, dirname
 import optparse
 
-def main():
-    path.append(join(abspath(dirname(__file__)), '..'))
+path.append(join(abspath(dirname(__file__)), '..'))
 
-    from Driver.User import User
+from Driver.User import User
 
-    user = User()
+user = User()
 
-    parser = optparse.OptionParser(
-        description = 'Add a vsftpd user',
-        epilog = 'By Stefan.Midjich@cygate.se',
-        usage = 'Usage: %prog [options] <username>'
-    )
+parser = optparse.OptionParser(
+    description = 'Add a vsftpd user',
+    epilog = 'By Stefan.Midjich@cygate.se',
+    usage = 'Usage: %prog [options] <username>'
+)
 
-    parser.add_option(
-        '-g', '--groups',
-        action = 'store',
-        metavar = 'sudo,staff',
-        type = 'string',
-        help = 'Comma-separated list of additional group memberships'
-    )
+parser.add_option(
+    '-g', '--groups',
+    action = 'store',
+    metavar = 'sudo,staff',
+    type = 'string',
+    help = 'Comma-separated list of additional group memberships'
+)
 
-    parser.add_option(
-        '-d', '--directory',
-        action = 'store',
-        metavar = '/home/user',
-        type = 'string',
-        help = 'Home directory of user'
-    )
+parser.add_option(
+    '-d', '--directory',
+    action = 'store',
+    metavar = '/home/user',
+    type = 'string',
+    help = 'Home directory of user'
+)
 
-    parser.add_option(
-        '-c', '--comment',
-        action = 'store',
-        metavar = 'User Name',
-        type = 'string',
-        help = 'User description'
-    )
+parser.add_option(
+    '-c', '--comment',
+    action = 'store',
+    metavar = 'User Name',
+    type = 'string',
+    help = 'User description'
+)
 
-    parser.add_option(
-        '-p', '--password',
-        action = 'store',
-        metavar = 'Secret2013',
-        type = 'string',
-        help = 'Password of user'
-    )
+parser.add_option(
+    '-p', '--password',
+    action = 'store',
+    metavar = 'Secret2013',
+    type = 'string',
+    help = 'Password of user'
+)
 
-    (opts, args) = parser.parse_args()
-
-    try:
-        username = args[0]
-    except:
-        parser.print_usage()
-        exit(1)
-
-    try:
-        user.adduser(username, opts.password, opts.directory, opts.groups, opts.comment)
-    except Exception as e:
-        print('Problem importing user=%s, groups=%s: %s' % (username, opts.groups.split(' '), str(e)), file=stderr)
-
-    print('Finished importing %s' % username)
+(opts, args) = parser.parse_args()
 
 try:
-    main()
-except KeyboardInterrupt as e:
-    print("Received interrupt, exiting...", file=stderr)
+    username = args[0]
+except:
+    parser.print_usage()
     exit(1)
+
+try:
+    user.adduser(username, opts.password, opts.directory, opts.groups, opts.comment)
+except Exception as e:
+    print('Problem importing user=%s, groups=%s: %s' % (username, opts.groups.split(' '), str(e)), file=stderr)
+
+print('Finished importing %s' % username)

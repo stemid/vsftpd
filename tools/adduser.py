ccdfa914a72f35ccb9cbf743fac11ada22793f7b
Stefan Midjich <swehack@gmail.com>
2013-11-15 09:38:13 +0100

    fixes

--- tools/adduser.py
+++ tools/adduser.py
@@ -60,6 +60,6 @@ except:
 try:
     user.adduser(username, opts.password, opts.directory, opts.groups, opts.comment)
 except Exception as e:
-    print('Problem importing user=%s, groups=%s: %s' % (username, opts.groups.split(' ').join(','), str(e)), file=stderr)
+    print('Problem importing user=%s, groups=%s: %s' % (username, opts.groups.split(' '), str(e)), file=stderr)
 
 print('Finished importing %s' % username)

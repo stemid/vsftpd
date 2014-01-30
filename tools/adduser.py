865a256ee9a47cd73610a2d4944a4df485b3e65d
Stefan Midjich <swehack@gmail.com>
2014-01-30 15:59:40 +0100

    testing new way to add accounts

--- tools/adduser.py
+++ tools/adduser.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 
 from __future__ import print_function
-from sys import path, exit, stderr
+from sys import path, exit, stderr, stdout, stdin
 from os.path import join, abspath, dirname
+from termios import ECHO, TCSADRAIN, tcgetattr, tcsetattr
+from random import randint
+from crypt import crypt
+import string
 import optparse
 
 path.append(join(abspath(dirname(__file__)), '..'))
@@ -21,6 +25,7 @@ parser.add_option(
     '-g', '--groups',
     action = 'store',
     metavar = 'sudo,staff',
+    default = '',
     type = 'string',
     help = 'Comma-separated list of additional group memberships'
 )
@@ -42,11 +47,26 @@ parser.add_option(
 )
 
 parser.add_option(
-    '-p', '--password',
+    '-p', '--prompt',
+    action = 'store_true',
+    dest = 'prompt',
+    help = 'Prompt for a password'
+)
+
+parser.add_option(
+    '--password',
     action = 'store',
     metavar = 'Secret2013',
+    dest = 'password',
     type = 'string',
     help = 'Password of user'
+)
+
+parser.add_option(
+    '-v', '--verbose',
+    action = 'store_true',
+    dest = 'verbose',
+    help = 'Verbose output'
 )
 
 (opts, args) = parser.parse_args()
@@ -54,11 +74,45 @@ parser.add_option(
 try:
     username = args[0]
 except:
+    parser.error('Must specify username argument')
     parser.print_usage()
     exit(1)
 
+if opts.prompt and opts.password:
+    parser.error('Options --prompt and --password are mutually exclusive.')
+    exit(1)
+
+if opts.prompt:
+    # Prompt for a password from stdin
+    password_prompt = ''
+    if opts.verbose:
+        password_prompt = 'Enter password: '
+
+    # Disable echo to stdout
+    stdin_fd = stdin.fileno()
+    old_stdin = tcgetattr(stdin_fd)
+    new_stdin = tcgetattr(stdin_fd)
+    new_stdin[3] = new_stdin[3] & ~ECHO
+    try:
+        tcsetattr(stdin_fd, TCSADRAIN, new_stdin)
+        password = raw_input(password_prompt)
+    finally:
+        tcsetattr(stdin_fd, TCSADRAIN, old_stdin)
+else:
+    password = opts.password
+
+if password:
+    # Generate salt for encryption
+    salt_chars = '/.' + string.ascii_letters + string.digits
+    salt_string = [salt_chars[randint(0, len(salt_chars))] for c in range(0, 8)]
+    salt = '$1$' + ''.join(salt_string) + '$'
+
+    # Encrypt password
+    encrypted_password = crypt(password, salt)
+
 try:
-    user.adduser(username, opts.password, opts.directory, opts.groups.split(','), opts.comment)
+    #user.adduser(username, opts.password, opts.directory, opts.groups.split(','), opts.comment)
+    print(username, encrypted_password, opts.directory, opts.groups.split(','), opts.comment)
 except Exception as e:
     print('Problem importing user=%s, groups=%s: %s' % (
         username.encode('utf-8'), 
@@ -66,4 +120,5 @@ except Exception as e:
         str(e)
     ), file=stderr)
 
-print('Finished importing %s' % username)
+if opts.verbose:
+    print('Finished importing %s' % username)
